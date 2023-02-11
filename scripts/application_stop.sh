4454f0d50acdbc9990775b0f06189f7530428db2
Asif Dar <asif.dar@lelafe.com>
2023-02-11 13:40:19 +0530

    events deploy

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-#install pm2
-npm install -g pm2
-
-#Stopping existing node servers
-echo "Stopping any existing node servers"
-pm2 stop
+# Stop the Node.js server using pm2
+pm2 stop server.js
---
+++ scripts/start_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Start the Node.js server using pm2
+pm2 start server.js
