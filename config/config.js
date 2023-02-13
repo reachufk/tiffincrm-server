ed800fe6b570bde04e9359c244e27afdc439005d
Asif Dar <asif.dar@lelafe.com>
2023-02-13 22:20:00 +0530

    serverless database instance

--- config/config.js
+++ config/config.js
@@ -3,5 +3,5 @@ module.exports = {
       "Aws_Access_Key": "AKIAWAU2OY5YELVGTXG5",
       "Aws_Secret_Key": "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",
       "Aws_Bucket_Name": "tiffin-storage",
-      "databaseConnection": "mongodb+srv://Asif:Asif.chap111@tiffindb.e0hsqfw.mongodb.net/TiffinAawDB"
+      "databaseConnection": "mongodb+srv://Asif:Asif.chap111@tiffinaawdb.owxh4.mongodb.net/TiffinAawDB"
 };
