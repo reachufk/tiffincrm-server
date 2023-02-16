42c4f627396d5919af287ce9a303e0aa5e455c61
Asif Dar <asif.dar@lelafe.com>
2023-02-16 11:54:25 +0530

    Revert "up"
    This reverts commit 9fd9b60fa763dae7451d9c41214560a501bd894b.

--- application/app.js
+++ application/app.js
@@ -1,30 +1,19 @@
-const express = require('express');
+const express = require('express')
 const app = express();
 var cors = require('cors');
 var mongoose = require("mongoose")
-const swaggerUi = require('swagger-ui-express');
 var bodyParser = require('body-parser');
-var environment = require('./config/config');
+var config = require('../config/config');
 const compression = require('compression');
-const swaggerDocument = require('./swagger-api-docs/swagger-doc');
-const routes= require('./application/routes-domain/domain-routes')
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(compression());
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 mongoose.set("strictQuery", false);
-mongoose.connect(environment.databaseConnection,
+mongoose.connect(config.databaseConnection,
       { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("connection to database sucessfully."))
       .catch((error) => console.log(error));
 
-const port = process.env.PORT || 3000;
-
-app.use(routes);
-
-app.listen(port, () => {
-      console.log(`server is listening on port ${port}`)
-})
-
+module.exports = app
