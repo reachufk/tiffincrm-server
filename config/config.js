b3872ee8fa3925dc22ad6b4566f221d0c11dd49e
Asif Dar <asif.dar@lelafe.com>
2023-02-20 23:06:45 +0530

    access-bucket

--- config/config.js
+++ config/config.js
@@ -1,10 +1,8 @@
 module.exports = {
       "SecretKey": "chilaikalaan",
-      "Aws_Access_Key": "AKIAWAU2OY5YELVGTXG5",
-      "Aws_Secret_Key": "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",
       "Aws_Bucket_Name": "tiffin-storage",
       "databaseConnection": "mongodb+srv://Asif:Asif.chap111@tiffinaawdb.owxh4.mongodb.net/TiffinAawDB",
-      "BUCKET_USER_ACCESS_KEY":"AKIAWAU2OY5YN44WABVS",
-      "BUCKET_USER_SECRET_KEY":"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",
+      "BUCKET_USER_ACCESS_KEY":"AKIAWAU2OY5YFUZRNN5C",
+      "BUCKET_USER_SECRET_KEY":"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",
 
 };
