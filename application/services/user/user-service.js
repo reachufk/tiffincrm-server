7a3d690da14f4e4261a225d92a6e4bb8aadcd9f6
shyyashowkat <shyxum96@gmail.com>
2023-03-08 20:00:42 +0530

    _

--- application/services/user/user-service.js
+++ application/services/user/user-service.js
@@ -200,11 +200,12 @@ exports.TodaysOrder = async (req, res) => {
 
 exports.GetTodaysSales = async (req, res) => {
       const today = new Date();
+      let totalSalesAmount = 0;
       try {
             today.setHours(0, 0, 0, 0);
-            const query = { created_date: { $gte: today, $lt: new Date(today.getTime() + 24 * 60 * 60 * 1000) } };
-            const sales = await CompletedOrderModel.find(query, { _id: 0, name: 1 });
-            let totalSalesAmount = 0;
+            const sales = await CompletedOrderModel.find({
+                  createdAt: { $gte: today, $lt: new Date(today.getTime() + 24 * 60 * 60 * 1000) }
+            });
             sales.forEach(doc => totalSalesAmount += doc.orderAmount);
             res.status(200).send({
                   data: totalSalesAmount,
