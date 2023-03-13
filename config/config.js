c50d7779c3e85f9c508e23824bbc0160df1fd6d2
AsifAhmadDar <darasif.cse@gmail.com>
2023-03-13 22:25:35 +0530

    rpay eys

--- config/config.js
+++ config/config.js
@@ -6,7 +6,6 @@ module.exports = {
       "databaseConnection": "mongodb+srv://Asif:Asif.chap111@tiffinaawdb.owxh4.mongodb.net/TiffinAawDB",
       "BUCKET_USER_ACCESS_KEY":"AKIAWAU2OY5YFUZRNN5C",
       "BUCKET_USER_SECRET_KEY":"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",
-      "Razorpay_Key_Id":"rzp_test_G1Ge4DzK3mWbcm",
-      "Razorpay_Key_Secret":"zfaY5PlPmN2x6zr9s4Te4GfQ"
-
+      "Razorpay_Key_Id":"rzp_test_2alsaQKjTooqSr",
+      "Razorpay_Key_Secret":"xwaZ13dzTkb4Iag9JkGIK4Cz"
 };
